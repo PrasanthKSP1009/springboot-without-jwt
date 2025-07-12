863847441bbf3abecb3c768901acf27bfd312128
Prasanth K S <ksprasanthksp10@gmail.com>
2025-07-12 20:52:26 +0530

    Hello

--- src/main/java/com/example/springbootfirst/repository/RolesRepository.java
+++ src/main/java/com/example/springbootfirst/repository/RolesRepository.java
@@ -2,9 +2,12 @@ package com.example.springbootfirst.repository;
 
 import com.example.springbootfirst.models.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public interface RolesRepository extends JpaRepository<Roles,Integer> {
     Optional<Roles> findByName(String name);
 }
